bd71501c51023177d52bce02c0ddfde019263afd
JungCheolHoon <cjswovkdnj12@naver.com>
2024-02-24 15:42:26 +0900

    PR Test

---
+++ src/test/java/com/blog/jysblog/config/PRTest.java
@@ -0,0 +1,12 @@
+package com.blog.jysblog.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PRTest {
+    @Test
+    void test() {
+        System.out.println("Test");
+    }
+}
