ee16274c652a8394a560e566320aa6d6d34f7475
JungCheolHoon <cjswovkdnj12@naver.com>
2024-02-24 16:21:38 +0900

    modify jasyptConfig

--- src/main/java/com/blog/jysblog/config/JasyptConfig.java
+++ src/main/java/com/blog/jysblog/config/JasyptConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class JasyptConfig {
 
-    @Value("${jasypt.encryptor.key}")
+    @Value("${jasypt.encryptor.key:encodingTestKey}")
     String key;
 
     @Bean(name = "jasyptStringEncryptor")
